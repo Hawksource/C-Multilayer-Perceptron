ee7b1a971897e6142b404fd5c5a0981a070ed2fe
Hawksource <41839372+Hawksource@users.noreply.github.com>
2022-11-03 21:59:13 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,13 +9,13 @@ using namespace std;
 class neuron
 {
     double weight;
-    int scale;
+    int Scale;
 public:
     double Accumulate;
     const void setWeight(double);
     double getWeight();
-    neuron();
-    neuron(double);
+    neuron(int);
+    neuron(double, int);
 };
 
 typedef vector<neuron> layer;
@@ -28,8 +28,12 @@ class network
                         // Layers[layer number][neuron number]
     double relu(double);
 public:
+    int Scale;
+    bool operator== (network);
+    bool operator< (network);
+    bool operator> (network);
     double cost;
-    network(vector<unsigned>);
+    network(vector<unsigned>, int);
     network(network&, double); // 'copy & slightly change' constructor
     void predict(vector<double>);
     void predict(vector<double>, vector<double>);
@@ -37,6 +41,22 @@ public:
 
 
 };
+
+class population
+{
+    //functions in general order of being called while training
+    void runData(vector<vector<double> >); //runs dataset through all networks in pop
+                                            //vector of vectors(for multiple data points)
+    void Sort(); //sorts networks by their cost
+    void cull(); //kills 90% of the worst networks
+    void repopulate(); //repopulates the 90% from offshoots of the 10%
+public:
+    void train(vector<double>, int); //takes a dataset and # of epochs
+    network best(); //returns best network, pop[0] once sorted.
+    population(int,vector<unsigned>);
+    vector<network> pop; //holds networks
+};
+
 
 //main
 int main()
@@ -55,43 +75,46 @@ int main()
     vector<double> gt;
     gt.push_back(5);
 
-    network n = network(t);
-    network cn = network(n, 1);
+    network n = network(t, 1);
+    network cn = network(n, 10);
     n.printNet();
     cn.printNet();
-    cout <<n.cost << endl;
     return 0;
 }
 
 
 //class functions
-neuron::neuron()
+neuron::neuron(int scale)
 {
-    scale = 1;
+    Scale = scale;
     int sign = rand()%2;
     double temp = rand();
-    while(temp>scale) temp/=10.0;
     if(sign) temp*=-1;
-    weight = temp;
+    weight = (temp*(Scale/(double)RAND_MAX));
     Accumulate = 0;
 }
-neuron::neuron(double Weight)
+neuron::neuron(double Weight, int scale)
 {
-    scale = 1;
+    Scale = scale;
     weight = Weight;
+    if (Weight>Scale) Weight= Scale;//might cause issues in the future with loading
+                                    //in a pretrained model
     Accumulate = 0;
 }
 const void neuron::setWeight(double Weight)
 {
     weight = Weight;
+    while(weight>Scale) weight-=Scale; //might cause issues in the future with loading
+                                        //in a pretrained model
 }
 double neuron::getWeight()
 {
     return weight;
 }
 
-network::network(vector<unsigned> Topology)
+network::network(vector<unsigned> Topology, int scale)
 {
+    Scale = scale;
     topology = Topology;
     cost = 0;
     numLayers = topology.size();
@@ -100,28 +123,39 @@ network::network(vector<unsigned> Topology)
     for(int i = 0;i<numLayers;i++)
     {
         Layers.push_back(layer());
-        for(unsigned j = 0;j<topology[i];j++) Layers.back().push_back(neuron()); //<= to add bias
+        for(unsigned j = 0;j<topology[i];j++) Layers.back().push_back(neuron(Scale)); //<= to add bias
     }
     cout << "Network constructed\n";
 }
 
 network::network(network &target, double learningRate)
 {
+    //takes learning rate (a number > 1; a percent, not decimal) and for each neuron
+    //in the old network, creates a new one within +-learningRate% of the old.
+    //so learningRate = 1 basically copies the old netork
+
+    Scale = target.Scale;
+    if(learningRate<1)
+    {
+        cout << "Error: Learning rate(percent) is less than 1.\n\n";
+    }
     topology = target.topology;
     cost = 0;
     numLayers = topology.size();
     for(int i = 0;i<numLayers;i++)
     {
-        int sign = rand()%2;
-        double temp = rand();
-        while(temp>(learningRate/100.0)) temp/=10.0;
-        if(sign) temp*=-1;
+
         Layers.push_back(layer());
 
         for(unsigned j = 0;j<topology[i];j++)
             {
+            int increaseOrDecrease = rand()%2;
+                double temp = rand()%(int)learningRate;
+                temp/=100;
+                if(increaseOrDecrease) temp = 1+temp;
+                else temp = 1-temp;
                 double tw = target.Layers[i][j].getWeight();
-                Layers.back().push_back(neuron(tw+temp)); //<= to add bias
+                Layers.back().push_back(neuron(tw*temp, Scale)); //<= to add bias
             }
     }
     cout << "Network constructed\n";
@@ -191,7 +225,7 @@ void network::predict(vector<double> input,vector<double> groundTruth)
         Layers[0][i].Accumulate = input[i];
     }
     //
-    for(int i = 0;i<numLayers-1;i++)//for every layer
+    for(int i = 0;i<numLayers-1;i++)//for every layer(excluding output layer)
     {
         for(unsigned j = 0;j<Layers[i].size();j++) //for every neuron in the current layer
         {
@@ -204,6 +238,7 @@ void network::predict(vector<double> input,vector<double> groundTruth)
             }
         }
     }
+    //cost is the euclidean distance from the prediction to the ground truth
     double costSquared = 0;
    for(unsigned i = 0;i<groundTruth.size();i++)
    {
@@ -221,8 +256,23 @@ void network::predict(vector<double> input,vector<double> groundTruth)
 
 double network::relu(double in)
 {
-    if(in<0) return in;
+    if(in<0) return 0;
     else return in;
+}
+bool network::operator== (network A)
+{
+    if(this->cost==A.cost) return true;
+    else return false;
+}
+bool network::operator< (network A)
+{
+    if(this->cost<A.cost) return true;
+    else return false;
+}
+bool network::operator> (network A)
+{
+    if(this->cost>A.cost) return true;
+    else return false;
 }
 void network::printNet()
 {
